76344ba402fe94e10e8717c486bf89dcbd4b0a0c
Pooja Ashok Jadhav <poojajadhav550777@gmail.com>
2026-01-01 15:40:49 +0530

    Add IAM role for AWS Glue

---
+++ .github/workflows/infrastructure/infrastructure/infrastructure/infrastructure/iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "glue_service_role" {
+  name = "glue-service-role-etl"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_s3_access" {
+  role       = aws_iam_role.glue_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
